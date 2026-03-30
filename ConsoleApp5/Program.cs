37810c0fa343ac5734fe8620f71c88769d40272b
nazriinn <nazrinmr@code.edu.az>
2026-03-30 20:46:15 +0400

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello,from Nazrin!");
+        }
+    }
+}
